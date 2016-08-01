2968ec09293eb53ecc0f5eb044ec46c48ba17406
Matthew Laubach <mclaubach@gmail.com>
2016-08-01 21:24:13 +0000

    changes

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ActiveRecord::Base
-  has_attached_file :artpic, styles: { medium: "300x300>", thumb: "15%" }, default_url: "/images/:style/missing.png"
+  has_attached_file :artpic, styles: { original: "", medium: "300x300>", thumb: "15%" }, default_url: "/images/:style/missing.png",
+    source_file_options: { all: '-auto-orient' }
+  
   validates_attachment_content_type :artpic, content_type: /\Aimage\/.*\Z/
 end
