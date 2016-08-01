192a1a88f75235c3f278513e8b139092cefa4918
Matthew Laubach <mclaubach@gmail.com>
2016-08-01 22:30:07 +0000

    aws

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 gem "paperclip", "~> 5.0.0"
 gem "devise"
+gem 'aws-sdk', '~> 2.3'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
   # Paperclip config:
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
   Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+
 end
